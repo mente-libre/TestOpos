66c5cd630df34e9c09f5c024118982044289de89
Mente Libre <todocuentasnhdo@gmail.com>
2025-09-14 23:29:39 +0000

    import Image from "next/image"; import logo from "@/public/logo.png"; //

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -4,7 +4,14 @@ import logo from '@/app/logo.png';
 export function Logo(props: React.HTMLAttributes<HTMLDivElement>) {
     return (
         <div {...props}>
-            <Image src={logo} alt="TestOpos logo" width={130} height={32} />
+            <Image 
+              src={logo} 
+              alt="TestOpos logo" 
+              width={130} 
+              height={32}
+              style={{ width: 'auto', height: 'auto' }}
+              priority
+            />
         </div>
     )
 }
