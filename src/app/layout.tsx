8ea529810bcaff5fea1b134cc841779c5298819a
jaime <todocuentasnhdo@gmail.com>
2025-09-18 00:12:26 +0200

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
+  themeColor: "#2563eb",
 };
 
 export default function RootLayout({
