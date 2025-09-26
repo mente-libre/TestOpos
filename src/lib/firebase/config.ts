43736c2a3bb7a333fb690f865c895f5d5cb04750
todocuentasnhdo <todocuentasnhdo@gmail.com>
2025-09-26 16:11:33 +0000

    Primer commit

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,17 +1,15 @@
-
 import { initializeApp, getApp, getApps, FirebaseApp } from "firebase/app";
 
 // This file is now only used for CLIENT-SIDE auth, not Firestore.
 // Firestore will be accessed via server actions.
 
 const firebaseConfig = {
-  projectId: "studio-4528893091-676f0",
-  appId: "1:675547564853:web:f9793d2b360402309b5a7c",
-  storageBucket: "studio-4528893091-676f0.firebasestorage.app",
-  apiKey: "AIzaSyAWQJQNadcpABNfvxGQXIEwRAAoDN-m9Iw",
-  authDomain: "studio-4528893091-676f0.firebaseapp.com",
-  messagingSenderId: "675547564853",
-  databaseURL: "https://studio-4528893091-676f0.firebaseio.com"
+  apiKey: "AIzaSyAM3JQNadcpABNfvxQQXTEwRAADN-e9Tw",
+  authDomain: "studio-4528893091-676f8.firebaseapp.com",
+  projectId: "studio-4528893091-676f8",
+  storageBucket: "studio-4528893091-676f8.firebasestorage.app",
+  messagingSenderId: "675547564053",
+  appId: "1:675547564053:web:f9793d2b360402309b5a7c"
 };
 
 // Initialize Firebase
