1a19307fe1b4fc89cb59ea4be1eca82a10f87eda
todocuentasnhdo <todocuentasnhdo@gmail.com>
2025-09-27 12:32:00 +0000

    3

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -4,12 +4,12 @@ import { initializeApp, getApp, getApps, FirebaseApp } from "firebase/app";
 // Firestore will be accessed via server actions.
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAM3JQNadcpABNfvxQQXTEwRAADN-e9Tw",
-  authDomain: "studio-4528893091-676f8.firebaseapp.com",
-  projectId: "studio-4528893091-676f8",
-  storageBucket: "studio-4528893091-676f8.firebasestorage.app",
-  messagingSenderId: "675547564053",
-  appId: "1:675547564053:web:f9793d2b360402309b5a7c"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
