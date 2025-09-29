fe0e33bae7683b3766ab2bb0e7ce84ad27b32a88
todocuentasnhdo <todocuentasnhdo@gmail.com>
2025-09-29 16:59:15 +0000

    3

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_AI_AVAILABLE: process.env.GOOGLE_API_KEY ? 'true' : '',
-  },
-};
-
-export default nextConfig;
