0ee417c5dc655a229dcc90a0c470cd8064f1e590
todocuentasnhdo <todocuentasnhdo@gmail.com>
2025-10-04 12:12:27 +0000

    Configurar el proyecto de Firebase correcto

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,14 +3,13 @@ import { initializeApp, getApp, getApps, FirebaseApp } from "firebase/app";
 // This file is now only used for CLIENT-SIDE auth, not Firestore.
 // Firestore will be accessed via server actions.
 
-// TEMPORARY DEBUGGING STEP: Hardcoding credentials to isolate environment variable issues.
 const firebaseConfig = {
   apiKey: "AIzaSyDBCmF-I1zB_B_3SEErilDfczpvIZ7P7IY",
-  authDomain: "studio-4528893091-16285.firebaseapp.com",
-  projectId: "studio-4528893091-16285",
-  storageBucket: "studio-4528893091-16285.firebasestorage.app",
-  messagingSenderId: "1018687891954",
-  appId: "1:1018687891954:web:9770511dd186c68647236b"
+  authDomain: "testopos-proyects.firebaseapp.com",
+  projectId: "testopos-proyects",
+  storageBucket: "testopos-proyects.appspot.com",
+  messagingSenderId: "590229889399",
+  appId: "1:590229889399:web:3154c131fc6eb423f8c5ca"
 };
 
 // Initialize Firebase
