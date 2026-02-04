9e4a8fb6fc950ea019008425e1bdb18f0c10b5e3
todocuentasnhdo <todocuentasnhdo@gmail.com>
2026-02-04 15:27:34 +0000

    Emergency: Space optimization

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,45 +1,16 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
-{pkgs}: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.11"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
-  packages = [
-    pkgs.nodejs_20
-    pkgs.zulu
-    pkgs.genkit
-  ];
-  # Sets environment variables in the workspace
-  env = {};
-  # This adds a file watcher to startup the firebase emulators. The emulators will only start if
-  # a firebase.json file is written into the user's directory
-  services.firebase.emulators = {
-    # Disabling because we are using prod backends right now
-    detect = false;
-    projectId = "demo-app";
-    services = ["auth" "firestore"];
-  };
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      # "vscodevim.vim"
-    ];
-    workspace = {
-      onCreate = {
-        default.openFiles = [
-          "src/app/page.tsx"
-        ];
-      };
+{ pkgs, ... }: {
+	# Which nixpkgs channel to use.
+	channel = "stable-23.11"; # or "unstable"
+	# Use https://search.nixos.org/packages to find packages
+	packages = [
+		pkgs.nodejs_20
+	];
+	# Sets environment variables in the workspace
+	env = {
+      NEXT_PUBLIC_FIREBASE_PROJECT_ID = "YOUR_PROJECT_ID_HERE";
+      GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL = "YOUR_CLIENT_EMAIL_HERE";
+      # IMPORTANT: Make sure to wrap the private key in single quotes (')
+      # if it contains special characters.
+      GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY = "YOUR_PRIVATE_KEY_HERE";
     };
-    # Enable previews and customize configuration
-    previews = {
-      enable = true;
-      previews = {
-        web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
-          manager = "web";
-        };
-      };
-    };
-  };
 }
